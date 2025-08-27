be309a434a17e578319f53abecc988f2222abbe7
parth desai <desaiparth182@gmail.com>
2025-08-27 18:42:23 +0530

    first commit from frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import Dashboard from './Components/Dashboard';
-import PrivateRoute from './utils/PrivateRoute';
-import { Toaster } from 'react-hot-toast';
+
 
 function App() {
   return (
-    <Router>
-      <Toaster position="top-center" reverseOrder={false} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      </Routes>
-    </Router>
+    <>
+      <p>hello world</p>
+    </>
     
   );
 }
